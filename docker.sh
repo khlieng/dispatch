d38daf976b444006d487fe7499793e1a54c24ab4
Ken-Håvard Lieng <kk_998@hotmail.com>
2015-06-12 04:21:17 +0200

    Set ldflags -w when compiling for docker

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp /etc/ssl/certs/ca-certificates.crt .
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o build/name_pending
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags -w -o build/name_pending
 docker build -t khlieng/name_pending .
