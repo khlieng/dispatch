24aa1193773638baac382ffa72287f9495772002
Ken-Håvard Lieng <kk_998@hotmail.com>
2016-01-05 19:33:45 +0100

    Remove pure decorator from the Root container

--- client/src/js/containers/Root.dev.js
+++ client/src/js/containers/Root.dev.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { Router } from 'react-router';
 import { Provider } from 'react-redux';
-import pure from 'pure-render-decorator';
 import DevTools from './DevTools';
 
-@pure
 export default class Root extends Component {
   render() {
     const { store, routes, history } = this.props;
--- client/src/js/containers/Root.prod.js
+++ client/src/js/containers/Root.prod.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { Router } from 'react-router';
 import { Provider } from 'react-redux';
-import pure from 'pure-render-decorator';
 
-@pure
 export default class Root extends Component {
   render() {
     const { store, routes, history } = this.props;
