0413960b852c126aa0aa8ae4ac1afdde670acd6b
Ken-Håvard Lieng <kk_998@hotmail.com>
2017-04-20 04:16:15 +0200

    Fix /help output

--- client/src/js/commands.js
+++ client/src/js/commands.js
@@ -1,4 +1,3 @@
-import _escape from 'lodash/escape';
 import createCommandMiddleware from './middleware/command';
 import { COMMAND } from './actions';
 import { setNick, disconnect, whois, away } from './actions/server';
@@ -20,7 +19,7 @@ const help = [
   '/whois <user> - Get information about user',
   '/away [message] - Set or clear away message',
   '/raw [message] - Send raw IRC message to the current server'
-].map(_escape);
+];
 
 export default createCommandMiddleware(COMMAND, {
   join({ dispatch, server }, channel) {
