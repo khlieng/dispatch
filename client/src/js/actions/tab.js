ff7c563b6111c8316dd34895db295942570ac2d0
Ken-Håvard Lieng <kk_998@hotmail.com>
2017-05-15 06:27:30 +0200

    Use replace in updateSelection

--- client/src/js/actions/tab.js
+++ client/src/js/actions/tab.js
@@ -1,11 +1,12 @@
 import * as actions from '../actions';
 import { push, replace } from '../util/router';
 
-export function select(server, name) {
+export function select(server, name, doReplace) {
+  const navigate = doReplace ? replace : push;
   if (name) {
-    return push(`/${server}/${encodeURIComponent(name)}`);
+    return navigate(`/${server}/${encodeURIComponent(name)}`);
   }
-  return push(`/${server}`);
+  return navigate(`/${server}`);
 }
 
 export function updateSelection() {
@@ -19,11 +20,11 @@ export function updateSelection() {
       dispatch(replace('/connect'));
     } else if (history.size > 0) {
       const tab = history.last();
-      dispatch(select(tab.server, tab.name));
+      dispatch(select(tab.server, tab.name, true));
     } else if (servers.has(server)) {
-      dispatch(select(server));
+      dispatch(select(server, null, true));
     } else {
-      dispatch(replace('/'));
+      dispatch(select(servers.keySeq().first(), null, true));
     }
   };
 }
