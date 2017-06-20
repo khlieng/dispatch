a4ebd8d4c4ad415a1df7c95400473856a61f3729
Ken-Håvard Lieng <kk_998@hotmail.com>
2017-06-20 04:26:18 +0200

    Update webpack to 3.0.0

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -23,6 +23,7 @@ module.exports = {
         NODE_ENV: JSON.stringify('production')
       }
     }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
