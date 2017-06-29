962f7d1eb0912e92b3f1aeac9c68b66849c1b0d8
Ken-Håvard Lieng <kk_998@hotmail.com>
2017-06-29 07:11:12 +0200

    Simplify Settings mapDispatch

--- client/src/js/containers/Settings.js
+++ client/src/js/containers/Settings.js
@@ -1,4 +1,3 @@
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import Settings from 'components/pages/Settings';
@@ -8,10 +7,10 @@ const mapState = createStructuredSelector({
   settings: getSettings
 });
 
-const mapDispatch = dispatch => ({
-  onCertChange(name, data) { dispatch(setCert(name, data)); },
-  onKeyChange(name, data) { dispatch(setKey(name, data)); },
-  ...bindActionCreators({ uploadCert }, dispatch)
-});
+const mapDispatch = {
+  onCertChange: setCert,
+  onKeyChange: setKey,
+  uploadCert
+};
 
 export default connect(mapState, mapDispatch)(Settings);
