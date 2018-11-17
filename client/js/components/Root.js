da87dccb538320bb286611cba0a0522287c1651c
Ken-Håvard Lieng <kk_998@hotmail.com>
2018-11-17 11:12:30 +0100

    Update react-hot-loader to 4.4.0, turn on pureSFC

--- client/js/components/Root.js
+++ client/js/components/Root.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { hot } from 'react-hot-loader';
+import { hot, setConfig } from 'react-hot-loader';
 import App from 'containers/App';
+
+setConfig({
+  pureSFC: true
+});
 
 const Root = ({ store }) => (
   <Provider store={store}>
