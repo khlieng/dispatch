edd4d6eadbf020ac00affd0c2a418764a25be792
Ken-Håvard Lieng <kk_998@hotmail.com>
2020-05-25 01:25:05 +0200

    Disable cgo in install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ Tag=$(git describe --tags)
 Commit=$(git rev-parse --short HEAD)
 Date=$(date +'%Y-%m-%dT%TZ')
 
-go install -ldflags "-s -w -X $Import.Tag=$Tag -X $Import.Commit=$Commit -X $Import.Date=$Date"
+CGO_ENABLED=0 go install -ldflags "-s -w -X $Import.Tag=$Tag -X $Import.Commit=$Commit -X $Import.Date=$Date"
